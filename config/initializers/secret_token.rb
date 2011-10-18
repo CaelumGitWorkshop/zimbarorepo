5a198972e42df8f6611efb7f1c9c914c1e8d8d34
Caelum Infra <infracaelumrj@gmail.com>
2011-10-18 20:13:09 -0200

    primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CadastroZimbaro::Application.config.secret_token = '4e8dd1127d807dd5dbdbf01a5f9fc55b174d30544f0a465be8763b557b15b679d4501320501b476ff478186983a076a3530fb33b312598a00cb841e74d916d09'
